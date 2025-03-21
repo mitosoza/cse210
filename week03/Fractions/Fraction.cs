69b8aff8cdb6329084869f28231be18d67d74610
mitosoza <msozagonzalez@byupathway.edu>
2025-03-20 22:49:01 -0600

    Week 03 - Learning Activity Encapsulation

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,57 @@
+using System;
+
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionalString = $"{_top}/{_bottom}";
+        return fractionalString;
+    }
+
+    public double GetDecimalValue()
+    {
+    double doubleFormat = (double)_top / _bottom;
+    return doubleFormat;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+        Fraction fraction4 = new Fraction();
+
+        fraction1.SetTop(1);
+        fraction1.SetBottom(1);
+        fraction2.SetTop(5);
+        fraction2.SetBottom(1);
+        fraction3.SetTop(3);
+        fraction3.SetBottom(4);
+        fraction4.SetTop(1);
+        fraction4.SetBottom(3);
+
+        Console.WriteLine($"{fraction1.GetFractionString()}");
+        Console.WriteLine($"{fraction1.GetDecimalValue()}");
+        Console.WriteLine($"{fraction2.GetFractionString()}");
+        Console.WriteLine($"{fraction2.GetDecimalValue()}");
+        Console.WriteLine($"{fraction3.GetFractionString()}");
+        Console.WriteLine($"{fraction3.GetDecimalValue()}");
+        Console.WriteLine($"{fraction4.GetFractionString()}");
+        Console.WriteLine($"{fraction4.GetDecimalValue()}");
     }
 }
