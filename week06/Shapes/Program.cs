6e0d5aab9000137f20e53cd10b5a2ce85834c620
mitosoza <msozagonzalez@byupathway.edu>
2025-04-10 14:03:45 -0600

    Week 06 - Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle("blue", 3, 5);
+        Circle circle = new Circle("yellow", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The shape has a color: {shape.GetColor()}, and area: {shape.GetArea()}");
+        }
     }
 }
